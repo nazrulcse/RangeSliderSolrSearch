4841832fc2a9c6467d03a8a920cd20d9bd3e402f
nazrul <nazrulku07@gmail.com>
2015-02-04 00:21:57 +0600

    generate pdf

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'coffee-rails', '~> 4.0.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'pdfkit'
+gem 'wkhtmltopdf'
+gem 'wkhtmltopdf-binary'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 gem 'jquery-turbolinks'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,14 @@ class WelcomeController < ApplicationController
   def index
 
   end
+
+  def generate_pdf
+    kit = PDFKit.new(params[:html], :page_size => 'Letter')
+    kit.stylesheets << Rails.root.join('app', 'assets', 'stylesheets', 'pdf.css')
+# Get an inline PDF
+    pdf = kit.to_pdf
+# Save the PDF to a file
+    file = kit.to_file('D://save_pdf/test.pdf')
+  end
+
 end
